0affeef1037d17c9204b8154f36ef1c1c67dd783
ehsan <savvyversa@gmail.com>
2022-12-19 17:13:08 +0330

    feat: separate item-decoration for timeline list

--- src/tmk.js
+++
@@ -1,8 +0,0 @@
-const daString =
-  "<div class='list-item-predot'></div><div class='list-item-dot'></div><div class='list-item-afterdot'></div>";
-const elements = document.querySelectorAll('.list-timeline .list-item');
-elements.forEach(function (item) {
-  // let item = elements[i];
-  console.log(elements, item);
-  item.innerHTML += daString;
-});

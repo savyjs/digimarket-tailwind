affa46ce8944c3e1f9dc2312c0a73db26c266726
ehsan <savvyversa@gmail.com>
2023-01-14 13:23:06 +0330

    feat: processing components and convert to css-in-js

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const plugin = require('tailwindcss/plugin');
+const autoprefixer = require('autoprefixer');
+const postcssNested = require('postcss-nested');
+const postcssImport = require('postcss-import');
+const postcssTailwincss = require('tailwindcss');
+const postcssTailwincssNesting = require('tailwindcss/nesting');
+// eslint-disable-next-line no-unused-vars
+const postcss = require('postcss');
 const postcssJs = require('postcss-js');
-const cssPath = './'
+const fs = require('fs');
+// eslint-disable-next-line no-unused-vars
 
 module.exports = plugin(
-  function ({ addUtilities, addComponents, theme, variants }) {
-    // const components = postcssJs.sync(cssPath);
-    // console.log({ components });
-    // addComponents(components);
+  async function ({ addUtilities, addComponents, theme, variants }) {
+    const css = fs.readFileSync(__dirname + '/src/all.css', 'binary');
+    const root = postcss.parse(css);
+    const cssFinal = await postcss([
+      autoprefixer,
+      postcssNested,
+      postcssImport({
+        root: __dirname + '/src',
+      }),
+      postcssTailwincss(require('./src/tailwind.config')),
+      postcssTailwincssNesting,
+    ]).process(root, {
+      from: 'src/all.css',
+      tp: 'dist/all.css',
+    });
+    const cssJs = postcssJs.objectify(root);
+
+    await fs.writeFileSync(__dirname + '/dist/all.css', cssFinal.css.toString(), 'binary');
+    if (cssFinal.map) {
+      fs.writeFile(
+        __dirname + '/dist/all.css.map',
+        cssFinal.map.toString(),
+        () => true
+      );
+    }
+    console.log({});
+
+    addComponents(cssJs);
     // If your plugin requires user config,
     // you can access these options here.
     // Docs: https://tailwindcss.com/docs/plugins#exposing-options
@@ -19,6 +51,7 @@ module.exports = plugin(
         display: 'block',
       },
     };
+    addUtilities(cssJs);
 
     // Conditionally add utility class based on user configuration.
     if (options.YOUR_PLUGIN_CUSTOM_OPTION) {

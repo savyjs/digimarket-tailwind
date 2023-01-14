cab76d7450844ff5039436a9ca5340ae798dba3a
ehsan <savvyversa@gmail.com>
2023-01-14 13:37:13 +0330

    fix: add tailwind plugins and theme config

--- index.js
+++ index.js
@@ -10,72 +10,41 @@ const postcssJs = require('postcss-js');
 const fs = require('fs');
 // eslint-disable-next-line no-unused-vars
 
-module.exports = plugin(
-  async function ({ addUtilities, addComponents, theme, variants }) {
-    const css = fs.readFileSync(__dirname + '/src/all.css', 'binary');
-    const root = postcss.parse(css);
-    const cssFinal = await postcss([
-      autoprefixer,
-      postcssNested,
-      postcssImport({
-        root: __dirname + '/src',
-      }),
-      postcssTailwincss(require('./src/tailwind.config')),
-      postcssTailwincssNesting,
-    ]).process(root, {
-      from: 'src/all.css',
-      tp: 'dist/all.css',
-    });
-    const cssJs = postcssJs.objectify(root);
+module.exports = plugin(async function ({
+  addUtilities,
+  addComponents,
+  theme,
+  variants,
+}) {
+  const css = fs.readFileSync(__dirname + '/src/all.css', 'binary');
+  const root = postcss.parse(css);
+  const cssFinal = await postcss([
+    autoprefixer,
+    postcssNested,
+    postcssImport({
+      root: __dirname + '/src',
+    }),
+    postcssTailwincss(require('./src/tailwind.config')),
+    postcssTailwincssNesting,
+  ]).process(root, {
+    from: 'src/all.css',
+    tp: 'dist/all.css',
+  });
+  const cssJs = postcssJs.objectify(root);
 
-    await fs.writeFileSync(__dirname + '/dist/all.css', cssFinal.css.toString(), 'binary');
-    if (cssFinal.map) {
-      fs.writeFile(
-        __dirname + '/dist/all.css.map',
-        cssFinal.map.toString(),
-        () => true
-      );
-    }
-    console.log({});
-
-    addComponents(cssJs);
-    // If your plugin requires user config,
-    // you can access these options here.
-    // Docs: https://tailwindcss.com/docs/plugins#exposing-options
-    const options = theme('tmk');
-
-    // Add CSS-in-JS syntax to create utility classes.
-    // Docs: https://tailwindcss.com/docs/plugins#adding-utilities
-    const utilities = {
-      '.example-utility-class': {
-        display: 'block',
-      },
-    };
-    addUtilities(cssJs);
-
-    // Conditionally add utility class based on user configuration.
-    if (options.YOUR_PLUGIN_CUSTOM_OPTION) {
-      utilities['.custom-utility-class'] = {
-        'background-color': 'red',
-      };
-    }
-
-    addUtilities(utilities, {
-      variants: variants('tmk'),
-    });
-  },
-  {
-    theme: {
-      // Default options for your custom plugin.
-      // Docs: https://tailwindcss.com/docs/plugins#exposing-options
-      tmk: {
-        YOUR_PLUGIN_CUSTOM_OPTION: false,
-      },
-    },
-    variants: {
-      // Default variants for your custom plugin.
-      // Docs: https://tailwindcss.com/docs/plugins#variants
-      tmk: ['responsive'],
-    },
+  await fs.writeFileSync(
+    __dirname + '/dist/all.css',
+    cssFinal.css.toString(),
+    'binary'
+  );
+  if (cssFinal.map) {
+    fs.writeFile(
+      __dirname + '/dist/all.css.map',
+      cssFinal.map.toString(),
+      () => true
+    );
   }
-);
+
+  addComponents(cssJs);
+  addUtilities(cssJs);
+});
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,3 +1,34 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  content: [{ raw: '' }],
+  content: [
+    __dirname + '/components/**/*.{scss,css,vue,jsx,html}',
+    __dirname + '/general/*.{scss,css,vue,jsx,html}',
+  ],
+  theme: {
+    extend: {
+      darkMode: true,
+      colors: {
+        transparent: 'transparent',
+        primary: '#EF4056',
+        secondary: '#008eb2',
+        error: '#f44336',
+        success: '#4caf50',
+        alert: '#ff9800',
+        dark: '#212121',
+        'dark-primary': '#131212',
+        'dark-accent': '#343232',
+        'light-accent': '#CCC',
+        light: '#EEE',
+        gray: colors.gray,
+        blue: colors.sky,
+        red: colors.rose,
+        pink: colors.fuchsia,
+      },
+      fontFamily: {
+        sans: ['IRANSans'],
+      },
+    },
+  },
+  plugins: [require('flowbite/plugin'), require('tw-elements/dist/plugin')],
 };

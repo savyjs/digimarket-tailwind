10e0c0d57432ab665fcf08f1fda8ff81b00ccadc
ehsan <savvyversa@gmail.com>
2023-01-15 09:38:09 +0330

    BREAKING CHANGE: rename index.js to plugin.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const merge = require('lodash/merge');
 const cssMatcher = require('jest-matcher-css');
 const postcss = require('postcss');
 const tailwindcss = require('tailwindcss');
-const customPlugin = require('./index');
+const customPlugin = require('./plugin');
 
 expect.extend({
   toMatchCss: cssMatcher,

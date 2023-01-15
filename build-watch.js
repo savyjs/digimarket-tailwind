1aa9e18d544804608933a5a6550f1bb997f461e4
ehsan <savvyversa@gmail.com>
2023-01-15 09:39:40 +0330

    feat: add builder-watch

--- build-watch.js
+++ build-watch.js
@@ -13,49 +13,49 @@ const css = fs.readFileSync(__dirname + '/src/all.css', 'binary');
 const root = postcss.parse(css);
 
 function reBuild() {
-    postcss([
-        autoprefixer,
-        postcssNested,
-        postcssGlobe,
-        postcssWatch({
-            folder: './src',
-            main: './src/all.css',
-        }),
-        postcssImport({
-            root: __dirname + '/src',
-        }),
-        postcssTailwincss(require('./src/tailwind.config')),
-        postcssTailwincssNesting,
-    ])
-        .process(root, {
-            from: 'src/all.css',
-            to: 'dist/all.css',
-        })
-        .then(result => {
-            const cssJs = postcssJs.objectify(result.root);
-            fs.writeFileSync(
-                './dist/components.json',
-                JSON.stringify(cssJs, null, 2)
-            );
-            console.info('built successfully!');
-        });
+  postcss([
+    autoprefixer,
+    postcssNested,
+    postcssGlobe,
+    postcssWatch({
+      folder: './src',
+      main: './src/all.css',
+    }),
+    postcssImport({
+      root: __dirname + '/src',
+    }),
+    postcssTailwincss(require('./src/tailwind.config')),
+    postcssTailwincssNesting,
+  ])
+    .process(root, {
+      from: 'src/all.css',
+      to: 'dist/all.css',
+    })
+    .then(result => {
+      const cssJs = postcssJs.objectify(result.root);
+      fs.writeFileSync(
+        './dist/components.json',
+        JSON.stringify(cssJs, null, 2)
+      );
+      console.info('built successfully!');
+    });
 }
 
 const chokidar = require('chokidar');
 
 const watcher = chokidar.watch(__dirname + '/src', {
-    ignored: __dirname + '/src/all.css', // ignore dotfiles
-    persistent: true,
+  ignored: __dirname + '/src/all.css', // ignore dotfiles
+  persistent: true,
 });
 reBuild();
 const log = console.log.bind(console);
 // Add event listeners.
 watcher
-    .on('change', path => {
-        log(`File ${path} has been changed`);
-        reBuild();
-    })
-    .on('unlink', path => {
-        log(`File ${path} has been removed`);
-        reBuild();
-    });
+  .on('change', path => {
+    log(`File ${path} has been changed`);
+    reBuild();
+  })
+  .on('unlink', path => {
+    log(`File ${path} has been removed`);
+    reBuild();
+  });

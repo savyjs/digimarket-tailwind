939a19ab01c5f78bb330a0fa328a65f2aa7042ed
ehsan <savvyversa@gmail.com>
2023-01-22 15:39:44 +0330

    fix: colors get removed - use default theme

--- build-watch.js
+++ build-watch.js
@@ -2,18 +2,18 @@ const build = require('./build');
 const chokidar = require('chokidar');
 
 const watcher = chokidar.watch(__dirname + '/src', {
-    ignored: __dirname + '/src/all.css', // ignore dotfiles
-    persistent: true,
+  ignored: __dirname + '/src/all.css', // ignore dotfiles
+  persistent: true,
 });
 build();
 const log = console.log.bind(console);
 // Add event listeners.
 watcher
-    .on('change', path => {
-        log(`File ${path} has been changed`);
-        build();
-    })
-    .on('unlink', path => {
-        log(`File ${path} has been removed`);
-        build();
-    });
+  .on('change', path => {
+    log(`File ${path} has been changed`);
+    build();
+  })
+  .on('unlink', path => {
+    log(`File ${path} has been removed`);
+    build();
+  });
